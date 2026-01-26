0b43807b3678181f45f5fcaad8e8cd7f3f8dbd39
Leonardo Silva <leonardosilva0156@gmail.com>
2026-01-26 17:47:05 -0300

    beta 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let tentativas = 1;
+const converter = document.getElementById('numero');
+const numeroconverttido = parseInt(converter, 10);
 
 function numeroAleatorio(min, max) {
   min = Math.ceil(min);
@@ -8,40 +9,34 @@ function numeroAleatorio(min, max) {
     
 const randomNumber = numeroAleatorio(1, 100);
 const numerogerado = Math.floor(Math.random() * 100) + 1;
-console.log(numerogerado)
+console.log(numerogerado);
 
 function verificar(){
-    const palpite = number(document.getElementById('palpite').value);
-    const mensagem = document.getElementById('mensagem');
-    tentativas++;
-
-    if(palpite === numerogerado){
-        alert('Parabéns, voçê acertou')
+    const palpite = document.getElementById('numero').value;
+    
+    if(palpite == numerogerado){
+        alert("Parabéns, voçê acertou");
     }
-    else if(palpite > numerogerado){
+    
+    else if(palpite > numerogerado) {
        alert("Numero alto, tente com um menor!");
     }
-    else{
-        alert("Numero baixo, tente com um maior! ")
+    
+    else { 
+        alert("Numero baixo, tente com um maior! ");
     }
+
+    
 }
 
 function resetarjogo(){
-    tentativas = 1;
-    const resetParas = document.querySelectorAll('.resultParas p');
-    for (const resetPara of resetParas) {
-        resetPara.textContent = '';
-    }
-
-    resetButton.parentNode.removeChild(resetButton);
+    const tentativas = 1;
+   
+    
    
 }
 
-const converter = document.getElementById("numero");
 
-const valorstring = converter.value;
-
-const valorInteiro = parseInt(valorstring, 30);
 
 
 
