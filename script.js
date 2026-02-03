9b4d653927bd52c4eaf0ea2eaaf4cde2397b5db0
Leonardo Silva <leonardosilva0156@gmail.com>
2026-02-02 22:02:28 -0300

    FIM 5.0

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+const tentativas = document.getElementById('tentativas');
+const mensagem = document.getElementById('mensagem');
+const button = document.getElementById('palpite');
+const input = document.getElementById('numero');
+
+const fulltentativas = 10;
+let tentativasRestantes = fulltentativas;
+
+
+tentativas.textContent = 'você tem ' + tentativasRestantes + ' tentativas';
+
 const converter = document.getElementById('numero');
 const numeroconverttido = parseInt(converter, 10);
 
@@ -11,51 +23,56 @@ const randomNumber = numeroAleatorio(1, 100);
 const numerogerado = Math.floor(Math.random() * 100) + 1;
 console.log(numerogerado);
 
+button.addEventListener('click', function () {
+    verificar();
+
 function verificar(){
     const palpite = document.getElementById('numero').value;
     
     if(palpite == numerogerado){
-        alert("Parabéns, voçê acertou");
+        mensagem.textContent = "Parabéns, voçê acertou";
+        tentativas.textContent = 'você tem ' + tentativasRestantes + ' tentativas';
+        button.disabled = true;
+        input.disabled = true;
+        return;
         
     }
     
     else if(palpite > numerogerado) {
-       alert("Numero alto, tente com um menor!");
+       mensagem.textContent = "Numero alto, tente com um menor!";
     }
     
     else { 
-        alert("Numero baixo, tente com um maior! ");
-
+        mensagem.textContent = "Numero baixo, tente com um maior! ";
     }
 
-    vidas();
     
-}
+    
+} 
 
-function vidas(){
-   let tentativas = 5;
-   while(tentativas > 0){
-    tentativas--;
-
-    if(tentativas > 0){
-    alert("você ainda tem " + tentativas + "tentativas");
-    break;
+    if(tentativasRestantes > 0){
+       tentativas.textContent = 'você tem ' + tentativasRestantes + ' tentativas';
+        
+    
    }
 
    else{
-    alert("Suas tentativas acabaram, o numero era " + numerogerado);
-    reiniciarjogo();
+    mensagem.textContent = "Suas tentativas acabaram, o numero era " + numerogerado;
+    button.disabled = true;
+    input.disabled = true;
+    return;
+    
    }
-  }
-}
+  
+       tentativasRestantes--;
+  
+       });
+});
+
 
 function reiniciarjogo(){
     location.reload();
 }
-
-
-
-    
 
     
 
